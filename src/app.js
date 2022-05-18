044f9dba4eaafc8e8da1760682eadee6cc7dc0af
Chris-Durr <chrisdurr88@gmail.com>
2022-05-18 19:19:40 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let primera = ["el", "una", "cinco", "sin"];
+let segunda = ["retorno", "casa", "pajaros", "ningun"];
+let tercera = ["delrey", "encantada", "volando", "sentido"];
+let body = document.querySelector("body");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let a = 0; a < primera.length; a++) {
+  for (let b = 0; b < segunda.length; b++) {
+    for (let c = 0; c < tercera.length; c++) {
+      {
+        body.innerHTML +=
+          "<h3> www." + primera[a] + segunda[b] + tercera[c] + ".es <br></h>";
+      }
+    }
+  }
+}
